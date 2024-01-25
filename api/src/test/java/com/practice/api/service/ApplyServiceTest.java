f24caf95c4500d7325b6542aa47d67a2858340d9
Park Yehan <parkyehan2060@gmail.com>
2024-01-25 15:38:50 +0900

    test: 1명이 쿠폰 여러 개 발급

--- api/src/test/java/com/practice/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/practice/api/service/ApplyServiceTest.java
@@ -55,4 +55,30 @@ public class ApplyServiceTest {
         assertThat(count).isEqualTo(100);
     }
 
+    @Test
+    public void 한명당_한개의_쿠폰만_발급() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드에서의 작업 기다리기
+
+        for (int i = 0; i < threadCount; ++i) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(1L); // 1번 유저가 쿠폰발급 요청을 1000번 보내도록
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Thread.sleep(10000); // consumer가 데이터를 수신해서 처리할 때까지의 대기 시간
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
 }
