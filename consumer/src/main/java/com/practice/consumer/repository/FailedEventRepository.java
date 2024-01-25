44ccba4f17c3d34fc5ab2c5cebc6e3e106998d13
Park Yehan <parkyehan2060@gmail.com>
2024-01-25 15:55:19 +0900

    feat: consumer에서 error 발생 시 로그 남기기

---
+++ consumer/src/main/java/com/practice/consumer/repository/FailedEventRepository.java
@@ -0,0 +1,7 @@
+package com.practice.consumer.repository;
+
+import com.practice.consumer.domain.FailedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
+}
